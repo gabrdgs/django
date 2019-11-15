67b2cc12930f58373cc62f5f35d08d1defcf0169
gabrdgs <gabbiematiias@gmail.com>
2019-11-14 22:04:46 -0300

    add template

--- amarelinha/urls.py
+++ amarelinha/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from landing import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.mostrar_index),
+    path('sobre/',views.mostrar_about)
+
 ]
